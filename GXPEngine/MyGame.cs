609a568e1c9116a73133545f5909c955758955dc
Quinten van Amstel <qvanamstel@gmail.com>
2023-05-09 14:39:00 +0200

    Main Menu added MyGame improved

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -1,22 +1,15 @@
 using System;                                   // System contains a lot of default C# libraries 
 using GXPEngine;                                // GXPEngine contains the engine
 using System.Drawing;                           // System.Drawing contains drawing tools such as Color definitions
+using System.Collections.Generic;
 
 public class MyGame : Game {
-	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
-	{
-		// Draw some things on a canvas:
-		EasyDraw canvas = new EasyDraw(800, 600);
-		canvas.Clear(Color.MediumPurple);
-		canvas.Fill(Color.Yellow);
-		canvas.Ellipse(width / 2, height / 2, 200, 200);
-		canvas.Fill(50);
-		canvas.TextSize(32);
-		canvas.TextAlign(CenterMode.Center, CenterMode.Center);
-		canvas.Text("Welcome!", width / 2, height / 2);
 
-		// Add the canvas to the engine to display it:
-		AddChild(canvas);
+	public int CurrentLevel;
+	public MyGame() : base(1280, 720, false)     // Create a window that's 800x600 and NOT fullscreen
+	{
+		
+
 		Console.WriteLine("MyGame initialized");
 	}
 
@@ -25,7 +18,37 @@ public class MyGame : Game {
 		// Empty
 	}
 
-	static void Main()                          // Main() is the first method that's called when the program is run
+	void LevelManagement()
+	{
+		switch (CurrentLevel)
+		{
+			case 1:
+				DestroyAll();
+				MainMenu menu = new MainMenu();
+				AddChild(menu);
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+
+		}
+
+	}
+
+    void DestroyAll()
+    {
+        List<GameObject> children = GetChildren(true);
+        for (int i = children.Count - 1; i >= 0; i--)
+        {
+            children[i].Destroy();
+
+        }
+    }
+
+    static void Main()                          // Main() is the first method that's called when the program is run
 	{
 		new MyGame().Start();                   // Create a "MyGame" and start it
 	}
---
+++ GXPEngine/levels/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using GXPEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    public class MainMenu : Sprite
+    {
+        public MainMenu() : base("Circle.png"){
+
+        }
+
+
+
+   
+}
+
+    
+
+
